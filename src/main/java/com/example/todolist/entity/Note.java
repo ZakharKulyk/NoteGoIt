a481cb33751aca9ca540dbe1d824694cc1d37cc9
zkhr <zakhar864@gmail.com>
2024-10-16 14:34:07 +0200

    Module 14

---
+++ src/main/java/com/example/todolist/entity/Note.java
@@ -0,0 +1,12 @@
+package com.example.todolist.entity;
+
+import lombok.Data;
+
+@Data
+public class Note {
+    int id;
+
+    String title;
+
+    String content;
+}
---
+++ src/main/java/com/example/todolist/exceptions/NoSuchNodeFound.java
@@ -0,0 +1,8 @@
+package com.example.todolist.exceptions;
+
+public class NoSuchNodeFound extends Exception {
+
+    public NoSuchNodeFound(String text){
+        super(text);
+    }
+}
